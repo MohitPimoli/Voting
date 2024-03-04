4c0f229ff2f47a8597837f0cbec41a1dcf477a74
Mohit <mohitpimoli31@gmail.com>
2024-03-04 11:42:14 +0530

    User login and Admin Login  has been added.

--- client/src/component/LoginPage/login.js
+++ client/src/component/LoginPage/login.js
@@ -1,31 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
 import './LoginPage.css';
-
 export default function LoginPage() {
   const history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [redirect, setRedirect] = useState(false);
-
-  useEffect(() => {
-    if (redirect) {
-      history.push("/AddCandidate");
-    }
-  }, [redirect, history]);
-
+  
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    if ((email === "mohitpimoli31@gmail.com" && password === "MyPassWord") || (email === "nb9690@gmail.com" && password === "MyPassWord123")) {
-      // If login is successful, redirect to admin page
-      setRedirect(true);
-    } else {
+  
+    // Check if the user is an admin
+    if (email === "rootadmin@voting.com" && password === "admin@123") {
+      history.push("/AddCandidate"); // Redirect to the admin page
+    } 
+    // Check if the user is a normal user
+    else if (email === "user@voting.com" && password === "password") {
+      history.push("/user"); // Redirect to the user page
+    } 
+    else {
       // Handle login failure
       alert("Login failed. Please check your email and password.");
     }
   };
+  
 
   return (
     <div className="login-page">
